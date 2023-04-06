5f7f06850688c2dbfddc44e485884c71f716e162
ricardo <ricardoduennas@gmail.com>
2023-04-06 08:54:02 -0500

    change background images

---
+++ src/funct2.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+            // Change background image of the page at load
+	var imageUrl = "../img/imgFeb/imgFeb"+ getRandomIntInclusive(101,264)+".jpeg";
+    $("body").css("background-image", "url(" + imageUrl + ")");
+    console.log(imageUrl);
+  });
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
